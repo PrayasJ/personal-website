3215b52cd38be8cd75859f1f6eea69cdaaff6449
Prayas Jain <prayas.jn24@gmail.com>
2023-08-29 16:25:44 +0530

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.scss'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className} style={{margin: 0}}>{children}</body>
+      <body className={inter.className} style={{margin: 0}}>{children}<Analytics /></body>
     </html>
   )
 }
