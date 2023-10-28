e252c0e0d074c0c72a4ab8e54e9dd1311b01d8a1
Prayas Jain <prayas.jn24@gmail.com>
2023-10-28 22:39:45 +0530

    added blogdata

---
+++ blogdata.js
@@ -0,0 +1,47 @@
+module.exports.BlogData = [
+    {
+        title: 'Selection to GSoC',
+        filePath: './blogs/GSoC - Week 1.md',
+        date: 'May 22, 2022'
+    },
+    {
+        title: 'Understanding Portal Project',
+        filePath: './blogs/GSoC - Week 2-3.md',
+        date: 'June 13, 2022'
+    },
+    {
+        title: 'Parallel Processing in R',
+        filePath: './blogs/GSoC - Week 4-5.md',
+        date: 'June 25, 2022'
+    },
+    {
+        title: 'Parallel Casting Function',
+        filePath: './blogs/GSoC - Week 6.md',
+        date: 'July 2, 2022'
+    },
+    {
+        title: 'Creating Node.js Packages',
+        filePath: './blogs/Creating Node.js packages.md',
+        date: 'July 11, 2022'
+    },
+    {
+        title: 'Add Windows Support',
+        filePath: './blogs/GSoC - Week 7-8.md',
+        date: 'July 16, 2022'
+    },
+    {
+        title: 'Testing the Project\'s Performance',
+        filePath: './blogs/GSoC - Week 9-10.md',
+        date: 'August 5, 2022'
+    },
+    {
+        title: 'Adding Documentation',
+        filePath: './blogs/GSoC - Week 11-12.md',
+        date: 'August 18, 2022'
+    },
+    {
+        title: 'GSoC Final Report',
+        filePath: './blogs/GSoC Final Report.md',
+        date: 'September 11, 2022'
+    }
+]
